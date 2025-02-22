deffee1f25d29e642d90256f25a0c8bcc3ecf3d7
Esther Ola <estherolowo2018@gmail.com>
2025-02-22 17:46:38 +0100

    new updatea

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
     <div className="bg-dark lg:flex lg:flex-row flex flex-col gap-3 lg:gap-[40rem] items-center text-white px-3 lg:py-3 py-2 footer">
       <div className="text-white">
         Copyright &copy; 2024 ||
-        <span className="text-gold textfooter"> designed by realtecheo</span>
+        <span className="text-gold textfooter"> designed by Esther-Ola</span>
       </div>
       <div className="">
         <SocioIcons></SocioIcons>
